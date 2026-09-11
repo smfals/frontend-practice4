b8eed7b992ad8508872ba9e9d91d446297069ed3
smfals <908399814@qq.com>
2026-09-11 18:56:36 +0800

    添加成绩数据定义

---
+++ score-stat/script.js
@@ -0,0 +1,10 @@
+
+const scores = [
+  { name: '李四', score: 92 },
+  { name: '王五', score: 45 },
+  { name: '赵六', score: 77 },
+  { name: '孙七', score: 59 },
+  { name: '周八', score: 88 },
+  { name: '吴九', score: 105 },   
+  { name: '郑十', score: -3 }     
+];
